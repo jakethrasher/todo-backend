9df89aa9be6be574513fc098142b4dc310b2653e
Jake Thrasher <mjakethrasher@gmail.com>
2021-03-01 14:11:39 -0800

    setp db

---
+++ data/todos.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    todo: 'must learn backend auth',
+    completed: false
+  },
+  {
+    todo: 'get comfortable with regex',
+    completed: false
+  },
+  {
+    todo: 'drink coffee',
+    completed: true
+  }
+];
